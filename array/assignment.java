84f5871420c8e4cc10e9e928ea0a79e6c1606ffa
Amit kumar <aniamitank@gmail.com>
2022-06-20 15:25:00 +0530

    assignment number 2

--- array/assignment.java
+++
@@ -1,7 +0,0 @@
-/*
- *  1- you have to store country currency value in 1D array
- *  2- create convertCurrency method which accept currency and index of exchange rate array,
- *     and return converted value.
- */
-public class assignment {
-}
